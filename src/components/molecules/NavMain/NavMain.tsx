830f63552e606e3c7f007ecfb30b0162dab90b78
deepa-simform <deepa.kumawat@simformsolutions.com>
2025-10-16 19:00:05 +0530

    sidebar update

--- src/components/molecules/NavMain/NavMain.tsx
+++ src/components/molecules/NavMain/NavMain.tsx
@@ -26,7 +26,12 @@ export function NavMain({
             <SidebarMenuItem key={item.title}>
               <Link
                 to={item.url}
-                className="flex items-center gap-2 w-full px-3 py-2 rounded hover:bg-muted transition-colors"
+                className="flex items-center gap-2 w-full px-3 py-2 rounded transition-colors"
+                activeProps={{
+                  "data-active": true,
+                  className:
+                    "bg-gray-200 text-sidebar-accent-foreground font-medium",
+                }}
               >
                 {item.icon && <item.icon />}
                 <span>{item.title}</span>
